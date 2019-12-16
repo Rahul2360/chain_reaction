12ab90ee90a85c3dea2afaaecac9a3b19a4a61e7
Rahul2360 <rahulbansal2360@gmail.com>
2019-12-16 17:51:11 +0530

    Draw lines dynamically

--- js/script.js
+++ js/script.js
@@ -1,46 +1,40 @@
-
-let row_count = 2;
-let column_count = 2;
+let row_count = 5;
+let column_count = 5;
 const lineWidth = 2;
-
-const starting_point_x = 0;
-const starting_point_y = 0;
 
 const box = {
 	height: 25,
 	width: 25
 };
 
-function getDimension() {
-	if (row_count != '' && column_count != '') {
-		reset();
-		column_count = document.getElementById('column_length').value;
-		row_count = document.getElementById('row_length').value;
-		draw();
-	}
-}
+$(document).ready(function () {
+
+	const starting_point_x = 0;
+	const starting_point_y = 0;
+
+	draw();
+});
 
 function draw() {
+	reset();
 	var canvas = document.getElementById('main-canvas');
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
-		
-		for (let i = 0; i< column_count; i++ ) {
-			for (let j = 0; j < row_count; j++ ) {
-				if (i%2 == j%2) {
-					ctx.fillStyle = 'black';
-					ctx.fillRect(starting_point_x +box.height*i ,starting_point_y+ box.width*j , box.height, box.width);
-				} else {
-					ctx.fillStyle = 'white';
-					ctx.fillRect(starting_point_x +box.height*i ,starting_point_y+ box.width*j , box.height, box.width);
-				}
-			}
+		for (let i = 1; i<= column_count; i++ ) {
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.moveTo(box.width*i, 0);
+			ctx.lineTo(box.width*i, box.width*row_count)
+			ctx.stroke();
+		}
+		for (let j = 1; j <= row_count; j++) {
+			ctx.lineWidth = lineWidth;
+			ctx.beginPath();
+			ctx.moveTo(0, box.height*j);
+			ctx.lineTo(box.height*column_count, box.height*j)
+			ctx.stroke();
 		}
 
-		// for (let i = 0; i< column_count; i++ ) {
-		// 	ctx.moveTo((box.width*i) + lineWidth, 0);
-		// 	ctx.lineTo((box.width*row_count))
-		// }
 	}
 }
 
@@ -48,12 +42,16 @@ function reset() {
 	var canvas = document.getElementById('main-canvas');
 	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
-		
-		for (let i = 0; i< column_count; i++ ) {
-			for (let j = 0; j < row_count; j++ ) {
-				ctx.fillStyle = 'white';
-				ctx.fillRect(starting_point_x +box.height*i ,starting_point_y+ box.width*j , box.height, box.width);
-			}
-		}
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+}
+
+
+function getDimension() {
+	if (row_count != null && column_count != null) {
+		// reset();
+		column_count = document.getElementById('column_length').value;
+		row_count = document.getElementById('row_length').value;
+		draw();
 	}
 }
