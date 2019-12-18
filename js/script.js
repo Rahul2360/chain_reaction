341aba24704f763208eb53835c0743bba947282c
Rahul2360 <rahulbansal2360@gmail.com>
2019-12-18 16:19:25 +0530

    store data in a json and code clean up

--- js/script.js
+++ js/script.js
@@ -23,6 +23,8 @@ const player = [
   }
 ];
 
+var matrix = [];
+
 $(document).ready(function () {
   draw();
   
@@ -40,9 +42,15 @@ function draw() {
 				ctx.rect(box.width*i, box.height*j, box.width, box.height);
 				ctx.lineWidth = lineWidth;
 				ctx.strokeStyle = "black";
-				ctx.stroke();
-			}
-		}
+        ctx.stroke();
+        var temp_obj = {};
+        temp_obj[i+ '_' + j] = {
+          value : false
+        };
+        matrix.push(temp_obj);
+      }
+    }
+    console.log(matrix);
 
 		// for (let i = 1; i<= column_count; i++ ) {
 		// 	ctx.lineWidth = lineWidth;
@@ -73,7 +81,7 @@ function getMousePosition(c, evt) {
 
 function handleClick(e) {
   var pos = getMousePosition(canvas, e);
-  var x,y;
+  var matrix_i,matrix_j;
   // for (let i = 0 ;i < column_count; i++) {
   //   if (pos.x > (box.width*i) &&  pos.x  < (box.width*(i + 1))) {
   //     x = i;
@@ -89,8 +97,8 @@ function handleClick(e) {
     for (let j = 0 ;j < row_count; j++) {
       if (pos.x > (box.width*i) &&  pos.x  < (box.width*(i + 1)) &&
       pos.y > (box.height*j) &&  pos.y < (box.height*(j + 1))) {
-        x = i;
-        y = j;
+        matrix_i = i;
+        matrix_j = j;
       }
     }
   }
@@ -100,8 +108,12 @@ function handleClick(e) {
   // console.log("******************************");
   // console.log((pos.y + lineWidth)/box.height);
   // console.log((pos.y - lineWidth)/box.height);
-  if (x != undefined && y != undefined) {
-    switch_player_turn(x, y);
+  if (matrix_i != undefined && matrix_j != undefined) {
+    var value = check_matrix(matrix_i,matrix_j);
+    if  (!value) {
+      fill_matrix(matrix_i,matrix_j);
+      switch_player_turn(matrix_i, matrix_j);
+    }
   }
   // if ( pos.x >  0 + lineWidth &&
   //   pos.x <  box.width + lineWidth && 
@@ -122,17 +134,35 @@ function switch_player_turn(x, y) {
     if (player[i].id == player_turn) {
       if (i != player.length-1) {
         player_turn = player[i+1].id;
-        ctx.fillStyle = player[i+1].color;
+        ctx.fillStyle = player[i].color;
         break;
       } else {
         player_turn = player[0].id;
-        ctx.fillStyle = player[0].color;
+        ctx.fillStyle = player[player.length - 1].color;
       }
     }
   }
   ctx.fillRect((x*box.width) + lineWidth, (y*box.height)+ lineWidth, box.width-(2*lineWidth), box.height-(2*lineWidth));
 }
 
+
+function check_matrix(j_value,i_value) {
+  var data_exist = false;
+  var index_i = parseInt(i_value*column_count + j_value);
+  var val = i_value + '_' + j_value;
+  if(matrix[index_i][val].value == true){
+    data_exist = true;
+  }
+  return data_exist;
+}
+
+
+function fill_matrix(j_value, i_value) {
+  var value = i_value + '_' + j_value;
+  var index_i = parseInt(i_value*column_count + j_value);
+  var key_name = Object.keys(matrix[index_i]);
+  matrix[index_i][key_name].value = true;
+}
 
 function reset() {
 	if (canvas.getContext) {
